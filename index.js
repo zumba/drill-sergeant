04af910c9ef8f53dc03dadac2a7c2d184a721b25
Chris Saylor <cjsaylor@gmail.com>
2013-12-05 08:10:28 -0500

    Implement commander and define repos and staletime.

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
-var Github = require('github');
-var _ = require('underscore');
+var _ = require('underscore'),
+	async = require('async'),
+	command = require('commander'),
+	pkg = require('./package.json'),
+	github = require('./lib/github');
 
-var ghClient = new Github({
-	version: '3.0.0',
-	protocol: 'https'
-});
+var ghClient, repos, entries = [], now = new Date().getTime();
 
-ghClient.authenticate({
-	type: 'oauth',
-	token: process.env.GITHUB_TOKEN
-});
+command
+	.version(pkg.version)
+	.option('-r, --repo [user/repository]', 'Define the repositories to check PRs. (Comma delimited)')
+	.option('-s, --staletime [number of hours]', 'Set the PR stale threshold. (default: 24)', 24)
+	.parse(process.argv);
 
-ghClient.pullRequests.getAll({
-	user: 'zumba',
-	repo: 'public',
-	state: 'open',
-	per_page: 1
-}, function(err, res) {
-	if (err) {
-		console.error(err);
+if (!process.env.GITHUB_TOKEN) {
+	console.error('GITHUB_TOKEN is required.');
+	process.exit(1);
+}
+
+if (!command.repo) {
+	console.error('Repo argument must be provided.');
+	process.exit(1);
+}
+
+repos = command.repo.split(',');
+
+ghClient = new github(process.env.GITHUB_TOKEN);
+
+async.map(
+	repos, 
+	function(repo, callback) {
+		ghClient.getPullRequests(repo, 'open', function(err, res) {
+			var result = {};
+			if (err) {
+				callback(err);
+			}
+			delete res.meta;
+			result.repo = repo;
+			result.prs = res;
+			callback(null, result);
+		});
+	},
+	function(err, res) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		_.each(res, function(repo) {
+			var prs;
+			if (repo.prs.length) {
+				var filtered;
+				prs = [];
+				filtered = _.filter(repo.prs, function(pr) {
+					return (now - new Date(pr.created_at).getTime()) / 1000 / 60 / 60 >= command.staletime;
+				})
+				if (filtered.length) {
+					_.each(filtered, function(pr) {
+						prs.push(_.pick(pr, ['created_at', 'html_url', 'title']));
+					});
+					entries.push({
+						repo: repo.repo,
+						prs: prs
+					});
+				}
+			}
+		});
+		console.log(entries);
 	}
-	_.each(res, function(data) {
-		var created = new Date(data.created_at).getTime();
-		var now = new Date().getTime();
-		console.log(created, now, (now - created) / 1000 / 60 / 60);
-		console.log({title: data.title, url: data.html_url, created: data.created_at});
-	});
-});
+);
